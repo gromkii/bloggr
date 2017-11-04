4e313cb1606c3ce6679ed25b0faf31a73cde0ee4
Dax <limeside@gmail.com>
2017-11-04 16:59:27 -0500

    Fix migrations, add seeds. :rabbit:

--- migrations/20171023155057_comments.js
+++ migrations/20171023155057_comments.js
@@ -1,20 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('comments')
-    .then(() => {
-      knex.schema.createTable('comments', table => {
-        table.increments().primary().index(),
-        table.text('comment'),
-        table.integer('post_id')
-          .references('id')
-          .inTable('posts')
-          .onDelete('cascade')
-        table.integer('user_id')
-          .references('id')
-          .inTable('users')
-          .onDelete('cascade'),
-        table.timestamp('created_at').defaultTo(knex.fn.now())
-    })
+  return knex.schema.createTable('comments', table => {
+    table.increments().primary().index(),
+    table.text('comment'),
+    table.integer('post_id')
+      .references('id')
+      .inTable('posts')
+      .onDelete('cascade')
+    table.integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('cascade'),
+    table.timestamp('created_at').defaultTo(knex.fn.now())
   })
 };
 
--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,25 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {username:'Jeff'},
+        {username:'Hank'},
+        {username:'Frank'}
       ]);
+    }).then(() => {
+      return knex('posts').insert([
+        {post_text:'Heya', user_id:1},
+        {post_text:'nm u', user_id:2},
+        {post_text:'Heya', user_id:3},
+      ])
+    }).then(() => {
+      return knex('comments').insert([
+        {comment:'fuck u', post_id:1, user_id:2},
+        {comment:'no fuck u', post_id:1, user_id:1},
+        {comment:'no fuck all you', post_id:1, user_id:3},
+      ])
     });
 };
